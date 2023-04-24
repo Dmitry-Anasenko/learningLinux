46ae721c4d2cf79858a88b2e7eb71f098f599262
dmitry <dmitry@debian>
2023-04-24 10:20:57 +1000

    Изменен дизайн главного экрана и экрана "О программе"

--- src/main/java/ru/hiik/learninglinux/AboutController.java
+++ src/main/java/ru/hiik/learninglinux/AboutController.java
@@ -6,6 +6,8 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -16,19 +18,24 @@ public class AboutController implements Initializable {
     
     @FXML
     private Label info = new Label();
-    
+    @FXML
+    private AnchorPane pane;
+    @FXML
+    private ImageView img;
     /**
      * Вывод информации о программе и среде выполнения
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        img.setPreserveRatio(false);
+        img.fitWidthProperty().bind(pane.widthProperty());
+        img.fitHeightProperty().bind(pane.heightProperty());
         App.stg.setTitle("Learning Linux - информация");
         info.setText("Learning Linux - программа для изучения операционных систем Linux\n\n"
         + "Рабочий каталог программы: " + System.getProperty("user.dir") + "\n\n"
         + "Домашний каталог Java Runtime Environment: " + System.getProperty("java.home") + "\n\n"
         + "Операционная система: " + System.getProperty("os.name")
-        + " " + System.getProperty("os.version")
-        + " " + System.getProperty("os.arch"));
+        + " " + System.getProperty("os.version"));
     }    
     
     @FXML
